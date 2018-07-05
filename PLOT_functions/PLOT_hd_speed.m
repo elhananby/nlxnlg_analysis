34d10519c16ba4ffb8c1d6dfa2267f6f89f2a50b
Unknown <elhananby@gmail.com>
2018-07-05 09:51:34 +0300

    update

--- PLOT_functions/PLOT_hd_speed.m
+++ PLOT_functions/PLOT_hd_speed.m
@@ -1,18 +1,33 @@
 subplot(m, n, count);
 
-speedIdxPos = vt.speed <= 1;
-speedIdxSpk = c.speed <= 1;
+hdBinsCalculate = -pi : 2*pi/nHdBins : pi;
+hdBinsPlot = linspace(-pi, pi, 60);
 
-hdBins = -pi : 2*pi/nHdBins : pi;
+vt.speed(vt.speed>50) = 50;
 
-hdRatesSlow = histcounts(c.poshd(speedIdxSpk), hdBins) ./ (histcounts(vt.poshd(speedIdxPos), hdBins) .* dt );
-hdRatesFast = histcounts(c.poshd(~speedIdxSpk), hdBins) ./ (histcounts(vt.poshd(~speedIdxPos), hdBins) .* dt);
+medSpeed = median(vt.speed);
 
-hdBins = linspace(-pi, pi, 60);
-polarplot(hdBins, cconv(hdRatesSlow./max(hdRatesSlow), win, nHdBins));
+hdRatesSlow = histcounts(c.poshd(c.speed <= medSpeed), hdBinsCalculate) ./...
+    (histcounts(vt.poshd(vt.speed <= medSpeed), hdBinsCalculate) .* dt );
+hdMeanSlow = circ_mean(hdBinsPlot', hdRatesSlow');
+hdScoreSlow = circ_r(hdBinsPlot', hdRatesSlow');
+
+hdRatesSlow(isnan(hdRatesSlow)) = 0;
+
+hdRatesFast = histcounts(c.poshd(c.speed > medSpeed), hdBinsCalculate) ./...
+    (histcounts(vt.poshd(vt.speed > medSpeed), hdBinsCalculate) .* dt);
+hdMeanFast = circ_mean(hdBinsPlot', hdRatesFast');
+hdScoreFast = circ_r(hdBinsPlot', hdRatesFast');
+
+hdRatesFast(isnan(hdRatesFast)) = 0;
+
+p1 = polarplot(hdBinsPlot, cconv(hdRatesSlow./max(hdRatesSlow), win, nHdBins), 'Color', [0 0.4470 0.7410]);
 hold on
-polarplot(hdBins, cconv(hdRatesFast./max(hdRatesFast), win, nHdBins));
+polarplot([hdMeanSlow hdMeanSlow], [0 hdScoreSlow], 'Color', [0 0.4470 0.7410], 'LineWidth', 3); 
+p2 = polarplot(hdBinsPlot, cconv(hdRatesFast./max(hdRatesFast), win, nHdBins), 'Color', [0.8500 0.3250 0.0980]);
+polarplot([hdMeanFast hdMeanFast], [0 hdScoreFast], 'Color', [0.8500 0.3250 0.0980], 'LineWidth', 3);
 
-legend('<= 1 cm/s', '> 1 cm/s', 'location', 'best');
+legend([p1 p2], {'Slow', 'Fast'}, 'location', 'best');
+title(['Median speed = ' num2str(medSpeed)]);
 
 count = count + 1;
